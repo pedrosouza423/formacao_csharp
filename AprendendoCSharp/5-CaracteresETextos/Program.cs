9c48a079e329d2c6361813e764d37427cdfbf440
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2021-08-24 19:56:20 -0300

    Foi feita a parte do curso de condicional

---
+++ AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5- Caracteres e texto");
+
+            char letra = 'p';
+            String nome = "Pedro";
+
+            Console.WriteLine(letra);
+            Console.WriteLine(nome);
+
+            char tabAsc = (char)65;
+            Console.WriteLine(tabAsc);
+
+
+            Console.WriteLine("Tecle enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AprendendoCSharp/7-Condicionais/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 7");
+
+            int idade = 16;
+            int quantidadePessoas = 3;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você é maior de idade. Pode entrar");
+            }
+            else if(idade >=16 && quantidadePessoas > 1)
+            {
+                Console.WriteLine("Você é menor de idade. Mas está acompanhado");
+
+            }
+            else
+            {
+                Console.WriteLine("Você é menor de idade");
+            }
+
+
+            Console.WriteLine("Tecle enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
